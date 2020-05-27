fb12ea3939fb05207bf97642180a331f97a7043b
Alonso Salas <alosalasv@gmail.com>
2020-05-26 22:41:01 -0400

    Initializing models

--- app/menus/models.py
+++ app/menus/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from dishes.models import Dish
 
 # Create your models here.
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateTimeField('date published')
+    dishes = models.ManyToManyField(Dish)
+
+    def __str__(self):
+        return self.name
--- app/orders/models.py
+++ app/orders/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from users.models import CustomUser
+from menus.models import Menu
 
 # Create your models here.
+
+
+class Order(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    menus = models.ManyToManyField(Menu)
+    note = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.totalPrice
