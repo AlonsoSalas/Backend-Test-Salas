1cd341ea71548ff5631cababc205521fcf4cf5b7
Alonso Salas <alosalasv@gmail.com>
2020-05-27 20:28:21 -0400

    Adding view method for user registration

--- app/users/views.py
+++ app/users/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from .serializers import RegistrationSerializer
+from rest_framework.authtoken.models import Token
 # Create your views here.
+
+
+class RegistrationAPIView(APIView):
+
+    def post(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+        data = {}
+        if serializer.is_valid():
+            new_user = serializer.save()
+            data['response'] = 'Sucessfully registered a new user'
+            token = Token.objects.get(user=new_user).key
+            data['token'] = token
+            return Response(data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
