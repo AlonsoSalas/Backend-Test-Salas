dc0f50b2e42e11baab9d9b93edb901d89b6fe829
Alonso Salas <alosalasv@gmail.com>
2020-05-30 23:07:22 -0400

    Adding serializers validators to orders create/update

--- app/orders/serializers.py
+++ app/orders/serializers.py
@@ -1,31 +1,44 @@
 from rest_framework import serializers
+from dishes.serializers import DishSerializer
+from menus.models import Menu
 from .models import Order
+from .models import Dish
+from rest_framework.exceptions import ValidationError, NotFound
 
 
 class OrderSerializer(serializers.ModelSerializer):
+    dishes = DishSerializer(many=True)
+
     class Meta:
         model = Order
-        fields = ['id', 'user', 'menu', 'note']
+        fields = ['id', 'user', 'menu', 'dishes', 'note']
         # fields = '__all__'
 
+    def create(self, validated_data):
+        menu = validated_data.get('menu')
+        if not menu.isAvailable():
+            raise ValidationError(
+                {'detail': f'The menu {menu.id}" is not Todays menu'})
 
-# class OrderSerializer(serializers.Serializer):
-#     name = serializers.CharField(max_length=100)
-#     description = serializers.CharField(max_length=200)
+        dishes = validated_data.pop('dishes')
+        validated_dishes = Menu.objects.validateDishes(
+            dishes, validated_data.get('menu'))
+        if not validated_dishes:
+            raise ValidationError(
+                {'detail': f'Your order must have at least one dish'})
+        order = Order.objects.create(**validated_data)
+        order.dishes.set(validated_dishes)
+        return order
 
-#     def create(self, validated_data):
-#         return Order.objects.create(validated_data)
+    def update(self, instance, validated_data):
+        instance.note = validated_data.get('note', instance.note)
+        instance.save()
 
-#     def update(self, instance, validated_data):
-#         instance.name = validated_data.get('name', instance.name)
-#         instance.description = validated_data.get(
-#             'description', instance.description)
-#         instance.ingredients = validated_data.get(
-#             'ingredients', instance.ingredients)
-#         instance.vegetarian = validated_data.get(
-#             'vegetarian', instance.vegetarian)
-#         instance.nongluten = validated_data.get(
-#             'nongluten', instance.nongluten)
-#         instance.price = validated_data.get('price', instance.price)
-#         instance.save()
-#         return instance
+        dishes = validated_data.pop('dishes')
+        Menu.objects.validateDishes(
+            dishes, instance.menu)
+
+        if dishes:
+            Order.objects.setDishes(dishes, instance)
+
+        return instance
