85c87d496fc354cb832e05a64a008529a01c4d1c
Alonso Salas <alosalasv@gmail.com>
2020-05-31 16:59:33 -0400

    Linking Dish manager with model

--- app/dishes/models.py
+++ app/dishes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .managers import DishManager
 
 # Create your models here.
 
@@ -6,6 +7,8 @@ from django.db import models
 class Dish(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
+
+    objects = DishManager()
 
     def __str__(self):
         return self.name
