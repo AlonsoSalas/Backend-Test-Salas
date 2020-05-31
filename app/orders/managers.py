7e46bc7734e80c805582c2fbc294a85d19f6354f
Alonso Salas <alosalasv@gmail.com>
2020-05-31 17:37:09 -0400

    removing setDishes method

--- app/orders/managers.py
+++ app/orders/managers.py
@@ -16,22 +16,3 @@ class OrderManager(models.Manager):
 
     def get_users_orders(self, username):
         return self.get_queryset().get_users_orders(username)
-
-    def setDishes(self, dishes, instance):
-        order = self.model.objects.get(id=instance.id)
-        if dishes:
-            Dish = apps.get_model('dishes', 'Dish')
-            keep_dishes = []
-            for dish in dishes:
-                dish_name = dish['name']
-                try:
-                    dish_instance = Dish.objects.get(
-                        name=dish_name, menu=instance.menu)
-                except ObjectDoesNotExist:
-                    raise NotFound(
-                        {'detail': f'There\'s no any dish with the id "{dish_name}" in order menu'})
-
-                keep_dishes.append(dish_instance)
-
-            order.dishes.set(keep_dishes)
-        return order
