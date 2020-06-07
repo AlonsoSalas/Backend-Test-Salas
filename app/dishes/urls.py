49e229ae7a06cf2abe888358eb196b80b712d463
Alonso Salas <alosalasv@gmail.com>
2020-06-07 13:15:44 -0400

    Simplifying  dishes view

--- app/dishes/urls.py
+++ app/dishes/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
-from .views import DishAPIView, DishDetails
+from .views import DishAPIView
 
 urlpatterns = [
     path('', DishAPIView.as_view()),
-    path('<uuid:id>/', DishDetails.as_view()),
+    path('<uuid:id>/', DishAPIView.as_view()),
 ]
--- app/dishes/views.py
+++ app/dishes/views.py
@@ -3,53 +3,63 @@ from .models import Dish
 from .serializers import DishSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import generics
+from rest_framework import mixins
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 # Create your views here.
 
 
-class DishAPIView(APIView):
+class DishAPIView(generics.GenericAPIView, mixins.ListModelMixin,
+                  mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin):
 
     permission_classes = [IsAuthenticated, IsAdminUser]
+    serializer_class = DishSerializer
+    queryset = Dish.objects.all()
 
-    def get(self, request):
-        menus = Dish.objects.all()
-        serializer = DishSerializer(menus, many=True)
-        return Response(serializer.data)
+    lookup_field = 'id'
+
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
 
     def post(self, request):
-        serializer = DishSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return self.create(request)
+
+    def put(self, request, id=None):
+        return self.update(request, id)
+
+    def delete(self, request, id=None):
+        return self.destroy(request, id)
 
 
-class DishDetails(APIView):
+# class DishDetails(APIView):
 
-    permission_classes = [IsAuthenticated, IsAdminUser]
+#     permission_classes = [IsAuthenticated, IsAdminUser]
 
-    def get_object(self, id):
-        try:
-            return Dish.objects.get(id=id)
+#     def get_object(self, id):
+#         try:
+#             return Dish.objects.get(id=id)
 
-        except Dish.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+#         except Dish.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
 
-    def get(self, request, id):
-        menu = self.get_object(id)
-        serializer = DishSerializer(menu)
-        return Response(serializer.data)
+#     def get(self, request, id):
+#         menu = self.get_object(id)
+#         serializer = DishSerializer(menu)
+#         return Response(serializer.data)
 
-    def put(self, request, id):
-        menu = self.get_object(id)
-        serializer = DishSerializer(menu, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def put(self, request, id):
+#         menu = self.get_object(id)
+#         serializer = DishSerializer(menu, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, id):
-        menu = self.get_object(id)
-        menu.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     def delete(self, request, id):
+#         menu = self.get_object(id)
+#         menu.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
