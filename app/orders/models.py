0e5b646545138a95f6fd9fcd3c0f66e876e555bd
Alonso Salas <alosalasv@gmail.com>
2020-06-08 12:04:33 -0400

    removing not used manager

--- app/orders/models.py
+++ app/orders/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.conf import settings
 from menus.models import Menu
 from dishes.models import Dish
-from .managers import OrderManager
 from common.models import BaseModel
 import uuid
 # Create your models here.
@@ -14,8 +13,6 @@ class Order(BaseModel):
     menu = models.ForeignKey(Menu, on_delete=models.SET_NULL, null=True)
     note = models.CharField(max_length=250)
     dishes = models.ManyToManyField(Dish)
-
-    objects = OrderManager()
 
     def __str__(self):
         return str(self.pk)
