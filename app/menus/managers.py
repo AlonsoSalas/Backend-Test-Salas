e5634314924311c5bda634745f0b6aa4d924f7b6
Alonso Salas <alosalasv@gmail.com>
2020-06-08 03:47:42 -0400

    Cleaning Menu manager unused code

--- app/menus/managers.py
+++ app/menus/managers.py
@@ -5,26 +5,11 @@ from rest_framework.exceptions import ValidationError, NotFound
 from django.core.exceptions import ObjectDoesNotExist
 
 
-class MenuQuerySet(models.QuerySet):
-    def getTodayMenu(self):
-        return self.get(date=datetime.date.today())
-
-    def MenuByDate(self, date):
-        return self.get(date=date)
-
-
 class MenuManager(models.Manager):
-    def get_queryset(self):
-        return MenuQuerySet(self.model, using=self._db)
-
     def getTodayMenu(self):
         try:
             menu = self.get(date=datetime.date.today())
-            menu.isAvailable()
             return menu
         except ObjectDoesNotExist:
             raise NotFound(
                 {'detail': 'There\'s no menu for today'})
-
-    def MenuByDate(self, date):
-        return self.get(date=date)
